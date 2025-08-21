a1101a2312d8e80dc78af8ac6c69032dd5ada3d1
kino <kino@pop-os.localdomain>
2025-08-21 17:33:39 +0200

    (\|) ._. (|/)

--- target/debug/deps/zero_shell-e5a9c2f62e5b1690.d
+++
@@ -1,8 +0,0 @@
-/home/kino/Videos/0-shell/target/debug/deps/zero_shell-e5a9c2f62e5b1690.d: src/main.rs src/shell.rs src/commands/mod.rs src/commands/mkdir.rs
-
-/home/kino/Videos/0-shell/target/debug/deps/zero_shell-e5a9c2f62e5b1690: src/main.rs src/shell.rs src/commands/mod.rs src/commands/mkdir.rs
-
-src/main.rs:
-src/shell.rs:
-src/commands/mod.rs:
-src/commands/mkdir.rs:
