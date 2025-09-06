614484a1857d85ddac8a39ec3f5016b2ccbe2ac9
zackoder <besszakria@gmail.com>
2025-09-06 08:39:22 +0100

    nothing to add

--- src/commands/cat.rs
+++ src/commands/cat.rs
@@ -1,11 +1,13 @@
+use std::env;
 use std::fs::File;
-use std::io::{Read};
+use std::io::Read;
 
 pub fn run(files: &[String]) {
     for file in files {
+        let mut path = file;
         match File::open(file) {
             Ok(mut f) => {
-                let mut buffer = String::new();
+                let mut buffer: String = String::new();
                 match f.read_to_string(&mut buffer) {
                     Ok(_) => print!("{}", buffer),
                     Err(e) => eprintln!("Failed to read {}: {}", file, e),
